2c6935ff8e7288ceca670b95d4a0cfe40fb449b8
Christian Fratta <christian@learnbrite.com>
2017-01-30 12:50:04 +0100

    Add some test stuff

---
+++ tests/ParserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class ParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSimple1()
+    {
+        $this->assertEquals(2, 1 + 2);
+    }
+
+    public function testSimple2()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
