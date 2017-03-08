100c53cae9400833ccab593d61efd3ae8ea98392
Christian Fratta <christian.fratta@gmail.com>
2017-03-08 07:55:27 +0100

    Sort array by number of mentions, and only show the first 10 on top10 list

--- app/Http/Controllers/NewsDayController.php
+++ app/Http/Controllers/NewsDayController.php
@@ -9,6 +9,22 @@ use Illuminate\Support\Facades\DB;
 class NewsDayController extends Controller
 {
     /**
+     * Save the current news day to the database.
+     *
+     * @param  $mentions
+     */
+    public static function saveNewsDay($mentions)
+    {
+        $newsDay = new NewsDay();
+        $newsDay->setAttribute('date', Carbon::today()->toDateString());
+        foreach ($mentions as $code => $number) {
+            $newsDay->$code = $number;
+        }
+
+        $newsDay->save();
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -28,23 +44,12 @@ class NewsDayController extends Controller
         $mentions = DB::table('news_data')->orderBy('date', 'desc')->first();
 
         $mentions = (array)$mentions;
+
         unset($mentions['recordId']);
         unset($mentions['date']);
-        return view('newsDay.top10')->with('mentions', $mentions);
-    }
 
-    /**
-     * Save the current news day to the database.
-     *
-     * @param  $mentions
-     */
-    public static function saveNewsDay($mentions) {
-        $newsDay = new NewsDay();
-        $newsDay->setAttribute('date', Carbon::today()->toDateString());
-        foreach($mentions as $code => $number) {
-            $newsDay->$code = $number;
-        }
+        arsort($mentions);
 
-        $newsDay->save();
+        return view('newsDay.top10')->with('mentions', array_slice($mentions, 0, 10));
     }
 }
