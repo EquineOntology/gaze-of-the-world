b798320b8b165bb790be37afaf9b59db2b3ceeed
Christian Fratta <christian.fratta@gmail.com>
2017-04-12 22:55:05 +0200

    Add news assimilation task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+	    $schedule->call('\CFratta\GazeOfTheWorld\Http\Controllers\GazeController@assimilateFeeds')
+		    ->daily()
+		    ->at('00:10');
     }
 
     /**
