61b3bb1358adc956fd4eac25145ce15b9a6529e2
Christian Fratta <christian.fratta@gmail.com>
2017-05-16 07:16:16 +0200

    Activate spatie/laravel-mailable-test

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Spatie\MailableTest\MailableTestServiceProvider::class,
 
         /*
          * Application Service Providers...
